96c5e86ab4c610c63855c362a6fff89ad83f15c7
sayheyBongmany <48705652+sayheyBongmany@users.noreply.github.com>
2022-02-19 22:59:35 +0900

    fix : Remove PortController.tsx

--- src/components/container/MainContainer.tsx
+++ src/components/container/MainContainer.tsx
@@ -2,14 +2,12 @@ import { memo } from "react";
 import { Box, Stack } from "@mui/material";
 import GraphContainer from "./GraphContainer";
 import TableContainer from "./TableContainer";
-import PortController from "../PortController";
 import ValueSender from "../ValueSender";
 
 function MainContainer(): JSX.Element {
 	return (
 		<Stack sx={{ alignItems: "center", width: "100%", height: "80%", marginY: "20px" }} direction="column" spacing="10px">
 			<Box sx={{ display: "flex", justifyContent: "space-between", width: "90%" }}>
-				<PortController />
 				<TableContainer />
 				<ValueSender />
 			</Box>
